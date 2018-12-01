7a58f39d7109bca04a2db97fe64ecbf39687e379
Sam Richard <sam.richard@gmail.com>
2018-12-01 06:22:42 -0700

    remove redux logging

--- packages/react-flying-saucer/lib/store/rematchPreset.js
+++ packages/react-flying-saucer/lib/store/rematchPreset.js
@@ -1,15 +1,5 @@
 import selectPlugin from '@rematch/select'
 
 export default {
-  redux: {
-    middlewares: [
-      store => next => action => {
-        console.log('dispatching', action)
-        let result = next(action)
-        console.log('next state', store.getState())
-        return result
-      },
-    ],
-  },
   plugins: [selectPlugin()],
 }
