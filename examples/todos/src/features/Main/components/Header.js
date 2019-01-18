bd028533764b069ce77f61428f028da8665dc631
Sam Richard <sam.richard@gmail.com>
2019-01-18 13:42:15 -0700

    use functions for all of Todos example

--- examples/todos/src/features/Main/components/Header.js
+++ examples/todos/src/features/Main/components/Header.js
@@ -1,5 +1,4 @@
-import { _$ } from '@@'
-import { Component } from 'react'
+import { _$, useHooks, useState } from '@@'
 
 const ENTER_KEY = 13
 
@@ -7,46 +6,39 @@ const enhance = _$(dispatch => ({
   addTodo: dispatch.todos.addTodo,
 }))
 
-class Header extends Component {
-  state = {
-    newTodo: '',
-  }
+function Header(props) {
+  const [newTodo, updateTodo] = useState('')
 
-  handleChange = event => {
-    this.setState({ newTodo: event.target.value })
-  }
-
-  handleNewTodoKeyDown = event => {
+  const handleChange = event => updateTodo(event.target.value)
+  const handleNewTodoKeyDown = event => {
     if (event.keyCode !== ENTER_KEY) {
       return
     }
 
     event.preventDefault()
 
-    const title = this.state.newTodo.trim()
+    const title = newTodo.trim()
 
     if (title) {
-      this.props.addTodo({ title })
-      this.setState({ newTodo: '' })
+      props.addTodo({ title })
+      updateTodo('')
     }
   }
 
-  render() {
-    return (
-      <header className="header">
-        <h1>todos</h1>
-        <input
-          ref="newField"
-          className="new-todo"
-          placeholder="What needs to be done?"
-          value={this.state.newTodo}
-          onKeyDown={this.handleNewTodoKeyDown}
-          onChange={this.handleChange}
-          autoFocus={true}
-        />
-      </header>
-    )
-  }
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <input
+        ref="newField"
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={newTodo}
+        onKeyDown={handleNewTodoKeyDown}
+        onChange={handleChange}
+        autoFocus={true}
+      />
+    </header>
+  )
 }
 
-export default Header |> enhance
+export default Header |> useHooks |> enhance
--- examples/todos/src/features/Main/components/TodoItem.js
+++ examples/todos/src/features/Main/components/TodoItem.js
@@ -1,5 +1,4 @@
-import { $$ } from '@@'
-import { Component } from 'react'
+import { $$, useHooks, useState, useRef, useEffect } from '@@'
 import { findDOMNode } from 'react-dom'
 import classNames from 'classnames'
 
@@ -20,97 +19,89 @@ const enhance = $$(
   })
 )
 
-class TodoItem extends Component {
-  state = { editText: this.props.todo.title }
+function TodoItem(props) {
+  const editFieldEl = useRef()
+  const [editText, updateEditText] = useState(props.todo.title)
 
-  handleSubmit = event => {
-    const title = this.state.editText.trim()
+  const handleSubmit = event => {
+    const title = editText.trim()
     if (title) {
-      this.props.save({ todo: { ...this.props.todo, title } })
-      this.setState({ editText: title })
+      props.save({ todo: { ...props.todo, title } })
+      updateEditText(title)
     } else {
-      this.props.onDestroy(this.props.todo)
+      props.onDestroy(props.todo)
     }
   }
 
-  handleEdit = () => {
-    this.props.onEdit()
-    this.setState({ editText: this.props.todo.title })
+  const handleEdit = () => {
+    props.onEdit()
+    updateEditText(props.todo.title)
   }
 
-  handleKeyDown = event => {
+  const handleKeyDown = event => {
     if (event.which === ESCAPE_KEY) {
-      this.setState({ editText: this.props.todo.title })
-      this.props.onCancel()
+      updateEditText(props.todo.title)
+      props.onCancel()
     } else if (event.which === ENTER_KEY) {
-      this.handleSubmit(event)
+      handleSubmit(event)
     }
   }
 
-  handleChange = event => {
-    if (this.props.isEditing) {
-      this.setState({ editText: event.target.value })
+  const handleChange = event => {
+    if (props.isEditing) {
+      updateEditText(event.target.value)
     }
-  }
-
-  /**
-   * This is a completely optional performance enhancement that you can
-   * implement on any React component. If you were to delete this method
-   * the app would still work correctly (and still be very performant!), we
-   * just use it as an example of how little code it takes to get an order
-   * of magnitude performance improvement.
-   */
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextProps.todo !== this.props.todo ||
-      nextProps.isEditing !== this.props.isEditing ||
-      nextState.editText !== this.state.editText
-    )
   }
 
   /**
    * Safely manipulate the DOM after updating the state when invoking
-   * `this.props.onEdit()` in the `handleEdit` method above.
+   * `props.onEdit()` in the `handleEdit` method above.
    * For more info refer to notes at https://facebook.github.io/react/docs/component-api.html#setstate
    * and https://facebook.github.io/react/docs/component-specs.html#updating-componentdidupdate
    */
-  componentDidUpdate(prevProps) {
-    if (!prevProps.isEditing && this.props.isEditing) {
-      var node = findDOMNode(this.refs.editField)
-      node.focus()
-      node.setSelectionRange(node.value.length, node.value.length)
-    }
-  }
+  useEffect(
+    () => {
+      if (editFieldEl.current && props.isEditing) {
+        // Put the cursor at the end of existing element
+        var node = findDOMNode(editFieldEl.current)
+        node.focus()
+        node.setSelectionRange(node.value.length, node.value.length)
+      }
+    },
+    [props.isEditing]
+  )
 
-  render() {
-    return (
-      <li
-        className={classNames({
-          completed: this.props.todo.completed,
-          editing: this.props.isEditing,
-        })}
-      >
-        <div className="view">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={this.props.todo.completed}
-            onChange={this.props.onToggle}
-          />
-          <label onDoubleClick={this.handleEdit}>{this.props.todo.title}</label>
-          <button className="destroy" onClick={this.props.onDestroy} />
-        </div>
+  return (
+    <li
+      className={classNames({
+        completed: props.todo.completed,
+        editing: props.isEditing,
+      })}
+    >
+      <div className="view">
         <input
-          ref="editField"
-          className="edit"
-          value={this.state.editText}
-          onBlur={this.handleSubmit}
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
+          className="toggle"
+          type="checkbox"
+          checked={props.todo.completed}
+          onChange={props.onToggle}
         />
-      </li>
-    )
-  }
+        <label onDoubleClick={handleEdit}>{props.todo.title}</label>
+        <button className="destroy" onClick={props.onDestroy} />
+      </div>
+      <input
+        ref={editFieldEl}
+        className="edit"
+        value={editText}
+        onBlur={handleSubmit}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+    </li>
+  )
 }
 
-export default TodoItem |> enhance
+/**
+ * The original demo makes a note of an optional performance enhancement.
+ * Using connect already makes components pure (memo).
+ */
+export default TodoItem |> useHooks |> enhance
