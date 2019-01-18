8b8d64270df5f5386f8dc18c378adae1ac94d07f
Sam Richard <sam.richard@gmail.com>
2019-01-18 13:43:21 -0700

    separate app bindings since they aren't router related

---
+++ packages/react-mothership/src/app/bindings.js
@@ -0,0 +1,11 @@
+import { useMemo } from 'use-react-hooks'
+
+import useApp from '../context'
+import useScope from '../scope'
+
+export const useNavigator = () => {
+  const app = useApp()
+  const { views } = useScope()
+
+  return useMemo(~app.navigate(views), [app, views])
+}
