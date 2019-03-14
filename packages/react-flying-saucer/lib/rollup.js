44b3c3cc84e7f08a218a21a61a39cc1262dce73a
Sam Richard <sam.richard@gmail.com>
2019-03-14 11:53:19 -0600

    Update rollup config (#11)
    * add jsx to rollup-plugin-node-resolve extensions
    * bundling rebases all css and sass to multiple entries
    * clean `dist` dir every bundle
    * create two bundles
     - esm bundle does not preserve modules; treeshaking still happens
     - umd bundle makes the process universal for any type of repo

--- packages/react-flying-saucer/lib/rollup.js
+++ packages/react-flying-saucer/lib/rollup.js
@@ -1,89 +1,105 @@
+const del = require('del')
 const path = require('path')
 const rollup = require('rollup')
+const { omit } = require('lodash')
 const { log } = require('@craco/craco/lib/logger')
 const { getLoader, loaderByName } = require('@craco/craco')
 
 const babel = require('rollup-plugin-babel')
-const commonjs = require('rollup-plugin-commonjs')
+const alias = require('rollup-plugin-alias')
+const rebase = require('rollup-plugin-rebase')
 const resolve = require('rollup-plugin-node-resolve')
-const minify = require('rollup-plugin-babel-minify')
 
 const { webpack } = require('../craco-config')
 
-const alias = aliases => ({
-  resolveId(importee) {
-    const alias = aliases[importee]
-
-    return alias ? this.resolveId(alias) : null
-  },
-})
-
-function bundle(name) {
-  return async configProvider => {
-    const { isFound, match } = getLoader(
-      configProvider,
-      loaderByName('babel-loader')
-    )
-
-    if (!isFound) {
-      throw new Error(
-        "craco: 'configProvider' does not contain a `babel-loader` configuration."
-      )
-    }
-
-    // remove webpack only config
-    const {
-      customize,
-      cacheDirectory,
-      cacheCompression,
-      cacheIdentifier,
-      ...options
-    } = match.loader.options
-
-    const inputOptions = {
-      input: 'src/index.js',
-      external: [
-        'react-flying-saucer',
-        'react-mothership',
-        'react',
-        'react-dom',
-        'lodash',
-      ],
-      plugins: [
-        alias(webpack.alias),
-        babel({
-          ...options,
-          runtimeHelpers: true,
-        }),
-        resolve({
-          jsnext: true,
-          main: true,
-          browser: true,
-        }),
-        commonjs(),
-        minify({
-          mangle: { topLevel: true },
-        }),
-      ],
-    }
-
-    const outputOptions = {
-      name,
-      file: 'dist/index.js',
-      format: 'umd',
-      sourcemap: true,
-      sourcemapFile: 'dist/index.js.map',
-    }
-
-    const bundle = await rollup.rollup(inputOptions)
-
-    log(`Bundling ${name} with dependencies:`)
-    log(bundle.watchFiles) // an array of file names this bundle depends on
-
-    await bundle.write(outputOptions)
+// dist is not ignored by SCM
+function getEsmOutputOptions() {
+  return {
+    file: 'dist/index.js',
+    format: 'esm',
+    sourcemap: true,
   }
 }
 
+function getUmdOutputOptions(name) {
+  return {
+    name,
+    file: 'dist/index.umd.js',
+    format: 'umd',
+    sourcemap: true,
+  }
+}
+
+function getBabelLoaderOptions(configProvider) {
+  const { isFound, match } = getLoader(
+    configProvider,
+    loaderByName('babel-loader')
+  )
+
+  if (!isFound) {
+    throw new Error(
+      "craco: 'configProvider' does not contain a `babel-loader` configuration."
+    )
+  }
+
+  // remove webpack only config
+  const options = omit(match.loader.options, [
+    'customize',
+    'cacheDirectory',
+    'cacheCompression',
+    'cacheIdentifier',
+  ])
+
+  return options
+}
+
+function getInputOptions(babelOptions) {
+  const extensions = ['.mjs', '.js', '.jsx', '.json']
+  return {
+    input: 'src/index.js',
+    onwarn: (warning, next) => {
+      // Ignore warnings in UMD about unnamed stylesheets
+      // and node_modules
+      if (warning.code === 'MISSING_GLOBAL_NAME') return
+      next(warning)
+    },
+    external: function(importee) {
+      // external if it doesn't start
+      // with a relative or absolute path
+      // assumes no one else can touch webpack.alias
+      return !/^([\.@]?\/|@@)/.test(importee)
+    },
+    plugins: [
+      alias({
+        resolve: ['/index.js', ...extensions],
+        ...webpack.alias,
+      }),
+      rebase(),
+      resolve(extensions),
+      babel({
+        ...babelOptions,
+        comments: false,
+        runtimeHelpers: true,
+      }),
+    ],
+  }
+}
+
+function bundle(name) {
+  return async configProvider => {
+    const babelOptions = getBabelLoaderOptions(configProvider)
+    const inputOptions = getInputOptions(babelOptions)
+
+    const bundle = await rollup.rollup(inputOptions)
+
+    log(`Bundling with dependencies:`)
+    log(bundle.watchFiles) // an array of file names this bundle depends on
+
+    await del('dist/*')
+    await bundle.write(getEsmOutputOptions())
+    await bundle.write(getUmdOutputOptions(name))
+  }
+}
 module.exports = {
   bundle,
 }
