1cef9b9f28d39277e4ae756a691f43704a519f39
Sam Richard <sam.richard@gmail.com>
2019-09-21 15:55:42 -0600

    change macro barrel to plain node syntax

--- packages/react-flying-saucer/macro.js
+++ packages/react-flying-saucer/macro.js
@@ -1,4 +1,7 @@
-export * from 'raw.macro'
-export * from 'hooks.macro'
-export * from 'param.macro'
-export * from 'z'
+Object.assign(
+  module.exports,
+  require('raw.macro'),
+  require('hooks.macro'),
+  require('param.macro'),
+  require('z')
+)
