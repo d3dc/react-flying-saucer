e58b571d9a5a6068d26ebc66c4ffb68a5196ee52
Sam Richard <sam.richard@gmail.com>
2019-09-21 15:54:56 -0600

    Use the latest dependencies

--- packages/bundle-react-flying-saucer/lib/rollup.js
+++ packages/bundle-react-flying-saucer/lib/rollup.js
@@ -1,14 +1,16 @@
 const del = require('del')
 const path = require('path')
 const rollup = require('rollup')
-const { last, omit } = require('lodash')
-const { log } = require('@craco/craco/lib/logger')
-const { getLoader, loaderByName } = require('@craco/craco')
 
 const babel = require('rollup-plugin-babel')
 const alias = require('rollup-plugin-alias')
 const rebase = require('rollup-plugin-rebase')
 const resolve = require('rollup-plugin-node-resolve')
+
+const { last, omit } = require('lodash')
+
+const { log } = require('@craco/craco/lib/logger')
+const { getLoader, loaderByName } = require('@craco/craco/lib/loaders')
 
 // dist is not ignored by SCM
 function getEsmOutputOptions() {
@@ -71,6 +73,7 @@ function getBabelLoaderOptions(webpackConfig, useESModules = true) {
 
 function getInputOptions(babelOptions, aliases = {}) {
   const extensions = ['.mjs', '.js', '.jsx', '.json']
+
   return {
     input: 'src/index.js',
     onwarn: (warning, next) => {
@@ -83,13 +86,15 @@ function getInputOptions(babelOptions, aliases = {}) {
       // real paths: /<module>, ./<module>, ../<module>
       // and aliases: @@, @/<module>
       const localImport = /^([.@]*\/|@@)/.test(importee)
-
       return !localImport
     },
     plugins: [
       alias({
         resolve: ['/index.js', ...extensions],
-        ...aliases,
+        entries: Object.entries(aliases).map(([key, value]) => ({
+          find: key,
+          replacement: value,
+        })),
       }),
       rebase(),
       resolve({
@@ -104,64 +109,59 @@ function getInputOptions(babelOptions, aliases = {}) {
   }
 }
 
-function bundle(name) {
-  return async (cracoConfig, webpackConfig, context) => {
-    const aliases = webpackConfig.resolve.alias
+async function bundle(name, webpackConfig) {
+  const aliases = webpackConfig.resolve.alias
 
-    const esmBundle = await rollup.rollup(
-      getInputOptions(getBabelLoaderOptions(webpackConfig), aliases)
-    )
+  const esmBundle = await rollup.rollup(
+    getInputOptions(getBabelLoaderOptions(webpackConfig), aliases)
+  )
 
-    log(`Bundling ESM with dependencies:`)
-    log(esmBundle.watchFiles) // an array of file names this bundle depends on
+  log(`Creating an ESM bundle from:`)
+  log(esmBundle.watchFiles) // an array of file names this bundle depends on
 
-    const umdBundle = await rollup.rollup(
-      getInputOptions(getBabelLoaderOptions(webpackConfig, false), aliases)
-    )
+  const umdBundle = await rollup.rollup(
+    getInputOptions(getBabelLoaderOptions(webpackConfig, false), aliases)
+  )
 
-    log(`Bundling UMD with dependencies:`)
-    log(umdBundle.watchFiles) // an array of file names this bundle depends on
+  log(`Creating an UMD bundle from:`, umdBundle.watchFiles) // an array of file names this bundle depends on
 
-    await del('dist/*')
-    await esmBundle.write(getEsmOutputOptions())
-    await umdBundle.write(getUmdOutputOptions(name))
-  }
+  await del('dist/*')
+  await esmBundle.write(getEsmOutputOptions())
+  await umdBundle.write(getUmdOutputOptions(name))
 }
 
-function watch(name) {
-  return async (cracoConfig, webpackConfig, context) => {
-    const aliases = webpackConfig.resolve.alias
+async function watch(name, webpackConfig) {
+  const aliases = webpackConfig.resolve.alias
 
-    await del('dist/*')
+  await del('dist/*')
 
-    const esmWatcher = rollup.watch({
-      ...getInputOptions(getBabelLoaderOptions(webpackConfig), aliases),
-      output: getEsmOutputOptions(),
+  const esmWatcher = rollup.watch({
+    ...getInputOptions(getBabelLoaderOptions(webpackConfig), aliases),
+    output: getEsmOutputOptions(),
+  })
+
+  const umdWatcher = rollup.watch({
+    ...getInputOptions(getBabelLoaderOptions(webpackConfig, false), aliases),
+    output: getUmdOutputOptions(),
+  })
+
+  for (const watcher of [esmWatcher, umdWatcher]) {
+    watcher.on('event', event => {
+      switch (event.code) {
+        case 'START':
+          return log('the watcher is (re)starting')
+        case 'BUNDLE_START':
+          return log('building an individual bundle')
+        case 'BUNDLE_END':
+          return log('finished building a bundle')
+        case 'END':
+          return log('finished building all bundles')
+        case 'ERROR':
+          return log('encountered an error while bundling')
+        case 'FATAL':
+          return log('encountered an unrecoverable error')
+      }
     })
-
-    const umdWatcher = rollup.watch({
-      ...getInputOptions(getBabelLoaderOptions(webpackConfig, false), aliases),
-      output: getUmdOutputOptions(),
-    })
-
-    for (const watcher of [esmWatcher, umdWatcher]) {
-      watcher.on('event', event => {
-        switch (event.code) {
-          case 'START':
-            return console.log('the watcher is (re)starting')
-          case 'BUNDLE_START':
-            return console.log('building an individual bundle')
-          case 'BUNDLE_END':
-            return console.log('finished building a bundle')
-          case 'END':
-            return console.log('finished building all bundles')
-          case 'ERROR':
-            return console.log('encountered an error while bundling')
-          case 'FATAL':
-            return console.log('encountered an unrecoverable error')
-        }
-      })
-    }
   }
 }
 
