8067e13b3a1cf19ef0b94ad5c828741b7ec34908
Sam Richard <sam.richard@gmail.com>
2019-09-28 20:16:54 -0600

    clarify overloaded craco config usage

--- packages/craco-flying-saucer/bootstrap.js
+++ packages/craco-flying-saucer/bootstrap.js
@@ -3,17 +3,20 @@ const fs = require('fs')
 
 const projectRoot = fs.realpathSync(process.cwd())
 const packageJsonPath = path.join(projectRoot, 'package.json')
-const configPath = path.relative(process.cwd(), __dirname)
+const backupPath = packageJsonPath + '.backup'
+const defaultConfigPath = path.relative(process.cwd(), __dirname)
 
-module.exports = function bootstrap(guarded) {
+module.exports = function bootstrap(guarded, configPath = defaultConfigPath) {
   const originalPackageData = fs.readFileSync(packageJsonPath)
   const packageJson = JSON.parse(originalPackageData)
 
   packageJson.cracoConfig = configPath
 
+  fs.renameSync(packageJsonPath, backupPath)
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson))
 
   guarded()
 
-  fs.writeFileSync(packageJsonPath, originalPackageData)
+  fs.unlinkSync(packageJsonPath)
+  fs.renameSync(backupPath, packageJsonPath)
 }
--- packages/react-flying-saucer/lib/craco-config.js
+++ packages/react-flying-saucer/lib/craco-config.js
@@ -1,5 +1,7 @@
 const baseConfig = require('craco-flying-saucer')
 
+// Automatically uses a single version of react-mothership (the one for this package)
+// in case a tool like lerna hasn't hoisted it.
 baseConfig.webpack.alias['react-mothership'] = require.resolve(
   'react-mothership'
 )
