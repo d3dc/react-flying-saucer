1380840eb676eee216afdca13216a2a2a4ee2737
Sam Richard <sam.richard@gmail.com>
2019-09-28 20:17:37 -0600

    use modified craco-config

--- packages/react-flying-saucer/bin/rfs.js
+++ packages/react-flying-saucer/bin/rfs.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-require('craco-flying-saucer/bootstrap')(() =>
-  require('@craco/craco/bin/craco.js')
+require('craco-flying-saucer/bootstrap')(
+  () => require('@craco/craco/bin/craco.js'),
+  require.resolve('../lib/craco-config')
 )
